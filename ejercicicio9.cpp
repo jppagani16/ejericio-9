7fab12e3e553fda1f546c304cae72e19423a8806
eliax92 <71987562+eliax92@users.noreply.github.com>
2020-09-27 16:58:35 -0300

    Create ejercicicio9.cpp

---
+++ ejercicicio9.cpp
@@ -0,0 +1,26 @@
+void IngresoFiesta (char tipo, string Date, int cant Qpersonas, int menu){
+
+}
+
+
+void MostrarResumenFiestas (){
+
+}
+
+void ContarFiestasxTipo (){
+
+}
+
+void MostrarQFiestasxTipo(){
+}
+
+int main(){
+cout<<"Por favor ingrese las fiestas planificadas (salir con -1 en fecha)"<<endl;
+IngresoFiesta ();
+MostrarResumenFiestas ();
+ContarFiestasxTipo();
+MostrarQFiestasxTipo();
+
+
+
+}
