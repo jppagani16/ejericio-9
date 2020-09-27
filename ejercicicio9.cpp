22025b8192391113e8eb77488699032242d2f233
jppagani16 <71987571+jppagani16@users.noreply.github.com>
2020-09-27 17:59:46 -0300

    Update ejercicicio9.cpp
    Te fijas que puede ser que no me funciona el módulo de ingreso? Cuando doy la opción de SALIR (FIESTA=2) sigue entrando al while

--- ejercicicio9.cpp
+++ ejercicicio9.cpp
@@ -1,4 +1,17 @@
-void IngresoFiesta (char tipo, string Date, int cant Qpersonas, int menu){
+void IngresoFiesta (char &tipo, string &Date, int &QPersonas, int &menu){
+	int FIESTA;
+	while (FIESTA=1){
+		cout<<"Ingrese Fecha de Fiesta: ";
+		cin>>Date;
+		cout<<endl<<"Ingrese Tipo de Fiesta (C, S, O): ";
+		cin>>tipo;
+		cout<<endl<<"Ingrese Cantidad de Personas de la fiesta: ";
+		cin>>QPersonas;
+		cout<<endl<<"Ingrese Tipo de Menu (1 o 2): ";
+		cin>>menu;
+		cout<<("Quiere ingresar otra fiesta para el mismo día (1-SI / 2-NO): ");
+		cin>>FIESTA;
+	}
 
 }
 
